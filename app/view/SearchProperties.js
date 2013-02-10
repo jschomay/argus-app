565701dc59e4858fd729ecbd9339118e55582b16
jschomay <jeffpublic@gmail.com>
2013-02-10 13:33:28 -0800

    nested attempt on search properties for detail card

--- app/view/SearchProperties.js
+++ app/view/SearchProperties.js
@@ -1,48 +1,62 @@
 Ext.define('ArgusApp.view.SearchProperties', {
-    extend: 'Ext.List',
+    // extend: 'Ext.List',
+    extend: 'Ext.navigation.View',
     xtype: "searchProperties",
     requires: [
         'Ext.data.Store',
-        'Ext.data.proxy.JsonP'
+        'Ext.data.proxy.JsonP',
+        'Ext.List'
     ],
     config: {
-        store: {
-            autoLoad: true,
-            fields: ['title', 'link', 'author', 'contentSnippet', 'content',
-                {
-                    name: 'leaf',
-                    defaultValue: true
+        items: [
+            {
+                xtype: 'list',
+                requires: [
+                    'Ext.data.Store',
+                    'Ext.data.proxy.JsonP'
+                ],
+                config: {
+                    store: {
+                        autoLoad: true,
+                        fields: ['title', 'link', 'author', 'contentSnippet', 'content',
+                            {
+                                name: 'leaf',
+                                defaultValue: true
+                            }
+                        ],
+                        root: {
+                            leaf: false
+                        },
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+                    itemTpl: '<b>{title}</b><br>{author}',
+                    // detailCard: {
+                    //     xtype: 'panel',
+                    //     scrollable: true,
+                    //     styleHtmlContent: true,
+                    //     html: "detail card placeholder"
+                    // },
+                    onItemDisclosure: true,
+                    // onItemDisclosure: function(){
+                    //     console.log("disclosed:", arguments);
+                    // },
+                    listeners: {
+                        select: function(view, record) {
+                            console.log("select", arguments);
+                            // this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
                 }
-            ],
-            root: {
-                leaf: false
-            },
-            proxy: {
-                type: 'jsonp',
-                url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
-                reader: {
-                    type: 'json',
-                    rootProperty: 'responseData.feed.entries'
-                }
+                
             }
-        },
-        itemTpl: '<b>{title}</b><br>{author}',
-        // detailCard: {
-        //     xtype: 'panel',
-        //     scrollable: true,
-        //     styleHtmlContent: true,
-        //     html: "detail card placeholder"
-        // },
-        onItemDisclosure: true,
-        // onItemDisclosure: function(){
-        //     console.log("disclosed:", arguments);
-        // },
-        listeners: {
-            select: function(view, record) {
-                console.log("select", arguments);
-                // this.getDetailCard().setHtml(post.get('content'));
-            }
-        }
+        ]
     }
 });
 
